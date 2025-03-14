46a9fe207db5cb5de4b22298c335a8c95291054f
Ebby Beltran <ebby.galle@gmail.com>
2025-03-14 15:09:01 -0500

    fix: console de ruta

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { store } from '../store';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 });
-
+console.log("API URL desde Render:", import.meta.env.VITE_API_URL);
 api.interceptors.request.use((config) => {
   let token = store.getState().auth.token;
 
